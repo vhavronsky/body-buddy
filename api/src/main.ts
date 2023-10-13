923a484fce1bcd78deb529ca8dd168df93ad3da1
vhavronsky <gavronskiy70@gmail.com>
2023-10-13 18:01:13 +0300

    feat(api): initial nestjs setup

---
+++ api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const PORT = process.env.PORT || 3000;
+
+  await app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}}`);
+  });
+}
+bootstrap();
